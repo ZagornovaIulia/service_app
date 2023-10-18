ad97b767de659826854f1ba77bc6d1f20f44ed1a
Iulia Zagornova <zagornowwa.julia@gmail.com>
2023-10-18 19:27:07 +0400

    add serializer

--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from services.serializers import SubscriptionSerializer
+from django.db.models import Prefetch
+from clients.models import Client
 
-# Create your views here.
+from services.models import Subscription
+
+class SubscriptionView(ReadOnlyModelViewSet):
+    queryset = Subscription.objects.all().prefetch_related(
+        'plan',
+        Prefetch('client',
+        queryset = Client.objects.all().select_related('user').only('company_name', 'user__email'))
+    )
+    serializer_class = SubscriptionSerializer
